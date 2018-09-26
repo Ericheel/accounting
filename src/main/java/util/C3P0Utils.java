6b35cefa283d4238672824ced5893580bd8d892d
0XCAFEBABE <283773833@qq.com>
2018-09-26 15:11:51 +0800

    XD

--- src/main/java/util/C3P0Utils.java
+++ src/main/java/util/C3P0Utils.java
@@ -2,6 +2,12 @@ package util;
 
 import com.mchange.v2.c3p0.ComboPooledDataSource;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  * C3P0连接池工具类
  *
@@ -11,5 +17,46 @@ public class C3P0Utils {
     private C3P0Utils() {
     }
 
-    private ComboPooledDataSource dataSource = new ComboPooledDataSource();
+    private static ComboPooledDataSource dataSource = new ComboPooledDataSource();
+
+    /**
+     * 获取连接池
+     *
+     * @return
+     */
+    public static DataSource getDataSource() {
+        return dataSource;
+    }
+
+    /**
+     * 获取连接对象
+     *
+     * @return
+     */
+    public static Connection getConnection() {
+        Connection connection;
+
+        try {
+            connection = dataSource.getConnection();
+            return connection;
+        } catch (Exception e) {
+            throw new RuntimeException("server busy...");
+        }
+    }
+
+    /**
+     * 释放连接
+     *
+     * @param connection
+     */
+    public static void releaseAll(Connection connection) {
+        if (null != connection) {
+            try {
+                connection.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            connection = null;
+        }
+    }
 }
